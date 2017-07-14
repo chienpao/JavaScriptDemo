c8f8cfffd5c1fd44ba6246bdd98fe2216b68d0de
Pao Chueh <chueh.pao@nuwarobotics.com>
2017-07-14 15:18:44 +0800

    test promise 2
    Use catch instead of reject() is more beautiful.

--- testPromise.js
+++ testPromise.js
@@ -4,15 +4,15 @@ let testPromise = new Promise((resolve, reject) => {
     console.log('in testPromise');
 
     // call resolve  
-    resolve('Resolve yo');
+    // resolve('Resolve yo');
 
     // call reject
-    // reject('Reject yo');
+    reject('Reject yo');
 });
 
 testPromise
-    .then((msg)=>{
+    .then((msg) => {
         console.log("YA! Resolve! The message is:" + msg);
-    }, (msg)=>{
-        console.log("SO SAD! Reject! The message is:" + msg);
+    }).catch((msg) => { 
+        console.log("WANNA CRY! Catch Error! The message is:" + msg);
     });
